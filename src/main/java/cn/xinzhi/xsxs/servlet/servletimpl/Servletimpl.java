bde33b9e882d319f80dc40161321a5012a97de51
lyx5 <y1378686795@163.com>
2019-12-25 16:06:04 +0800

    lyx555

--- src/main/java/cn/xinzhi/xsxs/servlet/servletimpl/Servletimpl.java
+++ src/main/java/cn/xinzhi/xsxs/servlet/servletimpl/Servletimpl.java
@@ -5,4 +5,7 @@ package cn.xinzhi.xsxs.servlet.servletimpl;
  * @create 2019-12-24 12:12
  */
 public class Servletimpl {
+    public void servlet(){
+        System.out.println("asdfghjk");
+    }
 }
